97f579f8b0a7877f58c61ff8948a5e62f6699ed2
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-07-21 15:13:03 +0530

    Initial Model Declaration

---
+++ file_services/app/file_models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, String, Integer, LargeBinary, DateTime, Text
+
+from .file_database import Base
+
+class File(Base):
+    __tablename__ = "files"
+
+    id = Column(Integer, primary_key=True)
+    file_name = Column(String, nullable=False, index=True)
+    file = Column(LargeBinary, nullable=False)
+    uploaded_at = Column(DateTime, nullable=False)
+
+class File_Details(Base):
+    __tablename__ = "file_details"
+
+    id = Column(Integer, primary_key=True)
+    file_name = Column(String, index=True, nullable=False)
+    file_path = Column(String, nullable=False, unique=True)
+    stored_filename= Column(String, nullable=False, index=True)
+    uploaded_at = Column(DateTime, nullable=False)
+    file_size = Column(Integer, nullable=False)
+    content = Column(Text)
