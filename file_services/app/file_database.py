c5dcbfc196e85791e7d9604e39f51294872ee37d
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-07-22 12:43:33 +0530

    Fix: Resolve asynchronous engine creation.

--- file_services/app/file_database.py
+++ file_services/app/file_database.py
@@ -1,10 +1,21 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, NullPool
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
 from file_services.app.file_config import settings
 
-engine = create_engine(settings.DATABASE_URL, connect_args={"check_same_thread": False})
-sessionLocal =  sessionmaker(bind=engine, autocommit=False, autoflush=False)
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    echo=True,
+    poolclass=NullPool
+    )
+sessionLocal =  sessionmaker(
+    bind=engine, 
+    autocommit=False, 
+    autoflush=False,
+    class_=AsyncSession,
+    expire_on_commit=False
+    )
 Base = declarative_base()
 
 def get_db():
