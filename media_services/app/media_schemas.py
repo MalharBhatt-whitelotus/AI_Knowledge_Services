9ebffd84e919ca7db48149982ad931eaa459cef3
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-07-22 16:01:23 +0530

    Feat: Implement initial schemas defination.

---
+++ media_services/app/media_schemas.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Enum
+from datetime import datetime
+from pydantic import BaseModel, Field
+
+class MediaType(str, Enum):
+    mp3 = "mp3"
+    mp4 = "mp4"
+
+class MediaDetailRequest(BaseModel):
+    id: int = Field(...)
+    media_id: int = Field(...)
+    media_name: str = Field(..., min_length=1, max_length=100)
+    media_size: int = Field(..., le=10)
+    media_type: MediaType = Field(...)
+    uploaded_at: datetime = Field(...)
+
+class MediaDetailResponse(BaseModel):
+    id: int
+    media_id: int
+    media_name: str
+    media_size: int
+    media_type: MediaType
+    uploaded_at: datetime
