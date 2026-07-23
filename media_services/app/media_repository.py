19b3a0a38b6a5f6c4e3f4a58101f96592b39551f
Malhar Bhatt <malhar@whitelotuscorporation.com>
2026-07-23 15:38:28 +0530

    Feat: - Implement update_media_details() repository method. - Implement update_media_file() repository method.

--- media_services/app/media_repository.py
+++ media_services/app/media_repository.py
@@ -3,12 +3,14 @@ from sqlalchemy import select, LargeBinary, text, func
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import BackgroundTasks
 from .media_models import Media, MediaDetail
-from .media_schemas import  MediaResponse,MediaDetailResponse
+from .media_schemas import  MediaResponse, MediaDetailRequest, MediaDetailResponse
+
 
 async def get_media_by_medianame(media_name: str, db: AsyncSession) -> MediaResponse | None:
     result = await db.execute(select(Media).where(Media.media_name == media_name))
     media = result.scalar_one_or_none()
     return media
+
 
 async def add_media_file(media_name: str, media_bytes: LargeBinary, db: AsyncSession) -> MediaResponse | None:
     media = Media(
@@ -21,6 +23,7 @@ async def add_media_file(media_name: str, media_bytes: LargeBinary, db: AsyncSes
     await db.refresh(media)
 
     return media
+
 
 async def add_media_details(media: MediaResponse, stored_name: str, media_path: str,media_size: int, media_type: str, uploaded_at: datetime, summary: str, db: AsyncSession) -> MediaDetailResponse:
     media_details = MediaDetail(
@@ -39,6 +42,7 @@ async def add_media_details(media: MediaResponse, stored_name: str, media_path: 
     await db.refresh(media_details)
 
     return media_details
+
 
 async def get_media_by_id(id: int, db: AsyncSession) -> MediaDetailResponse | None:
     result = await db.execute(select(MediaDetail).where(MediaDetail.id == id))
@@ -72,6 +76,37 @@ async def get_all_media_details(db: AsyncSession)-> list[MediaDetailResponse]:
     result = await db.execute(select(MediaDetail))
     media_details = result.scalars().all()
     return media_details
+
+
+async def update_media_details(id: int, details: MediaDetailRequest, db: AsyncSession) -> MediaDetailResponse:
+    result = await db.execute(select(MediaDetail).where(MediaDetail.id == id))
+    media_details = result.scalar_one_or_none()
+    media_details.id = id
+    media_details.media_id = details.media_id
+    media_details.media_name = details.media_name
+    media_details.stored_medianame = details.stored_medianame
+    media_details.media_path = details.media_path
+    media_details.media_size = details.media_size
+    media_details.media_type = details.media_type
+    media_details.uploaded_at = details.uploaded_at
+    media_details.uploaded_at = details.uploaded_at
+
+    await db.commit()
+    await db.refresh(media_details)
+    return media_details
+
+
+async def update_media_file(media_details: MediaDetailResponse, db: AsyncSession) -> MediaResponse:
+    result = await db.execute(select(Media).where(Media.id == media_details.media_id))
+    media = result.scalar_one_or_none()
+    media.id = media_details.media_id
+    media.media_name = media_details.media_name
+
+    await db.commit()
+    await db.refresh(media)
+
+    return media
+
 
 
 
